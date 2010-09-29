b2f6b342ea8b3c9c0d4b416d7e93586c8fc64d6b
JBennett <jbennett@ccistudios.com>
2010-09-29 10:44:47 -0400

    Added read more link to slideshow

--- public_html/templates/ccistudios/html/mod_k2_content/Slideshow/default.php
+++ public_html/templates/ccistudios/html/mod_k2_content/Slideshow/default.php
@@ -14,6 +14,10 @@ defined('_JEXEC') or die('Restricted access');
 <div id="k2ModuleBox<?php echo $module->id; ?>" class="slideshow k2ItemsBlock <?php echo $params->get('moduleclass_sfx'); ?>">
 	<?php if(count($items)): ?>
     <?php foreach ($items as $key=>$item):	?>
+	    <?php
+	    	$_a = $item->extra_fields[0]->value;
+	    	$_t = $item->extra_fields[1]->value;
+		?>
 	    <div class="item">				
 				<div class="image">
 					<img src="<?php echo $item->image; ?>" alt="<?php echo $item->title; ?>" />
@@ -21,9 +25,14 @@ defined('_JEXEC') or die('Restricted access');
 				
 				<div class="description">
 					<?php echo $item->introtext; ?>
+					<?php if ($_a) echo " <a href=\"$_a\" target=\"$_t\">Read more...</a>" ?>
 				</div>
-			</a>
 	    </div>
+	    <?php
+	    	if ($_a) {
+	    		echo "</a>";
+	    	}
+	    ?>
     <?php endforeach; ?>
 	<?php endif; ?>	
 </div>
