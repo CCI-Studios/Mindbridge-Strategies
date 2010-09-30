1a33d406c5337ecd23bd8724761bb1e7189c0604
JBennett <jbennett@ccistudios.com>
2010-09-30 11:08:31 -0400

    Added search image button and fixed IE layout

--- public_html/templates/ccistudios/index.php
+++ public_html/templates/ccistudios/index.php
@@ -33,7 +33,7 @@ $testing = true;
 	<link rel="shortcut icon" href="/templates/<?php echo $this->template ?>/images/favicon.ico" type="image/ico" />
 </head>
 
-<body class="<?php echo $menu; ?>">
+<body class="<?php echo 'page-'.$menu; ?>">
 <?php if (!$testing): ?>
 <script type="text/javascript">
   var _gaq = _gaq || [];
